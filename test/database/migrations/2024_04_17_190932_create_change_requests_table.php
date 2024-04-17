bcdebdda27ff3f992e2f7feeb925289afeea893f
jafarmh <jafar.m@yahoo.com>
2024-04-17 23:05:44 +0330

    database

---
+++ test/database/migrations/2024_04_17_190932_create_change_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('change_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string("email");
+            $table->decimal("amount",20);
+            $table->foreign("currency_from")->references('id')->on("currencies");
+            $table->foreign("currency_to")->references('id')->on("crm_drivers");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('change_requests');
+    }
+};
